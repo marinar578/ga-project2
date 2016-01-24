1ea249c940125b044ccecba8ba88b9a5713bc745
Marina Rozova <marinar578@gmail.com>
2016-01-24 15:15:07 -0500

    add search by title functionality

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
-  /* Menus */
 
+    // menus in nav
     var menu_items = $('#current-user');
 
     menu_items.hover(function(){
@@ -13,13 +13,33 @@ $(document).ready(function(){
     });
 
 
-// add category box
-
+    // add category box
     var category = $('#category');
 
     category.click(function(){
         $(".cat").show();
     })
 
+    // search by title
+    $('body').on('keyup', function(event){
+        if(event.which==13){       
+            var userInput= $('#search').val();
+            var userId = $('#'+userInput);
+
+            if (userInput.length > 0) {
+                $('.article_titles').hide();
+                    // userId.show();
+
+                for(i=0; i<$('.article_titles').length; i++){
+                   if (($('.article_titles')[i]["id"]).match(userInput)) {
+                     $('.article_titles:nth-child('+ (i+1) +')').show();
+                   };
+                };
+            };
+            if (userInput.length == 0) {
+                $('.article_titles').show();
+            };
+        };
+    });
 
 });
